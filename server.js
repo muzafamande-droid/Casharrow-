871873952bb77668314001a10fc5d9703fa2052d
muzafamande-droid <muzafamande@gmail.com>
2026-08-28 17:59:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
 
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/api/status", (req, res) => {
+res.json({
+success: true,
+message: "CashArrow server is running"
+});
+});
+
+app.get("*", (req, res) => {
+res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+console.log("CashArrow is running on port ${PORT}");
+});
