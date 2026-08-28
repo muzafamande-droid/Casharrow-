cf6f6635f225ffb868a0c18845186fd23a470552
muzafamande-droid <muzafamande@gmail.com>
2026-08-28 19:36:36 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const path = require("path");
+const db = require("./database");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -12,6 +14,28 @@ app.get("/api/status", (req, res) => {
 res.json({
 success: true,
 message: "CashArrow server is running"
+});
+});
+
+app.get("/api/tasks", (req, res) => {
+const tasks = db.prepare(
+"SELECT id, title, reward, done FROM tasks ORDER BY id DESC"
+).all();
+
+res.json({
+success: true,
+tasks
+});
+});
+
+app.get("/api/rewards", (req, res) => {
+const rewards = db.prepare(
+"SELECT id, title, amount, claimed FROM rewards ORDER BY id DESC"
+).all();
+
+res.json({
+success: true,
+rewards
 });
 });
 
