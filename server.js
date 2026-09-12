18d6fb8897cb925ce83d4a06123c9599d7ab1ebb
muzafamande-droid <muzafamande@gmail.com>
2026-09-12 18:15:47 +0300

    Expose rental earnings details API

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const app = require("./server-production");
 const db = require("./database-pg");
 const rental = require("./rental-routes");
+const rentalEarningsApi = require("./rental-earnings-api");
 const { startRentalEarningsWorker } = require("./rental-earnings-worker");
 
 const PORT = Number(process.env.PORT || 3000);
+
+app.use(rentalEarningsApi);
 
 async function start() {
   await db.init();
